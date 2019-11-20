cc9d569b699a56cf2802a1a10db7fbfaadb048b5
Piotr Limanowski <piotr@codearsonist.com>
2019-11-20 15:41:28 +0100

    git message

--- src/main.rs
+++ src/main.rs
@@ -10,26 +10,19 @@ fn main() {
         .version("1.0")
         .about("Align commit name with GitHub Issue")
         .arg(
-            Arg::with_name("www")
-                .required(true)
-                .help("Repository owner")
-                .index(1)
+            Arg::with_name("message-file")
+                .help("File containing commit message")
         )
         .arg(
             Arg::with_name("owner")
-                .required(true)
                 .help("Repository owner")
-                .index(2)
         )
         .arg(
             Arg::with_name("repo")
-                .required(true)
                 .help("Repository name")
-                .index(3)
         )
         .arg(
             Arg::with_name("token")
-                .required(false)
                 .help("GitHub user token")
         )
         .get_matches();
@@ -38,21 +31,23 @@ fn main() {
     let env_t = env::var(gh_token_key).unwrap();
 
     let t = matches.value_of("token").or(Some(&env_t)).expect("No token set");
-    let client = Github::new(t).expect("failed to create client");
     let o = matches.value_of("owner").expect("No owner set");
     let r = matches.value_of("repo").expect("No repo set");
+    let m = matches.value_of("message-file").expect("No message file set");
     
-    run(&client, o, r);
+    println!("file: {}, owner: {}, repo: {} token: {}", m, o, r, t);
+    let client = Github::new(r).expect("failed to create client");
+
+    run(&client, t, m, o, r)
 }
 
 
-fn run(client: &Github, owner: &str, repo_name: &str) -> () {
+fn run(client: &Github, message_file: &str, owner: &str, repo_name: &str, token: &str) -> () {
     let issues = get_issues(&client, owner, repo_name).expect("failed to get issues");
-
 
     let commit_msg = String::from_utf8(
         Command::new("cat")
-            .arg(".git/COMMIT_EDITMSG")
+            .arg(&message_file)
             .arg("| head -n 1")
             .output()
             .expect("Failed to execute git!")
